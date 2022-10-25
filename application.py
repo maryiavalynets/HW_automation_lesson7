86ca1c1a82efaa34abb7a050675e6443df5cd3dc
Maryia Valynets <maryia.valynets@gmail.com>
2022-10-25 13:10:22 -0700

    Add application.py

---
+++ application.py
@@ -0,0 +1,12 @@
+from pages.main_page import MainPage
+from pages.sign_in_page import SignInPage
+from pages.cart_page import CartPage
+
+
+class Application:
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.main_page = MainPage(self.driver)
+        self.sign_in_page = SignInPage(self.driver)
+        self.cart_page = CartPage(self.driver)
